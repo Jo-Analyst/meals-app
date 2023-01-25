30c91527a1c51a01e810dfafc7688191366d9e0a
Joelmir Rogério Carvalho <joalves3carvalho@gmail.com>
2023-01-25 06:23:12 -0300

    meals-app

---
+++ lib/main.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+main() => runApp(const MealsApp());
+
+class MealsApp extends StatelessWidget {
+  const MealsApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: "DeliMeals",
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('DeliMeals'),
+      ),
+      body: const Center(
+          child: Text(
+        'Navegar é preciso!!',
+      )),
+    );
+  }
+}
